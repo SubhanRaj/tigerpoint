ca9b39389a6b3e3600294f5f541e03e289e4fb33
Subhan Raj <shubhanraj2002@gmail.com>
2023-07-09 13:09:52 +0530

    Collections all

---
+++ app/Http/Controllers/CollectionsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Collections;
+use App\Http\Requests\StoreCollectionsRequest;
+use App\Http\Requests\UpdateCollectionsRequest;
+
+class CollectionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCollectionsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Collections $collections)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Collections $collections)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCollectionsRequest $request, Collections $collections)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Collections $collections)
+    {
+        //
+    }
+}

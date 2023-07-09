2e633e2e701be7a18ffcae915759514b39da21b2
Subhan Raj <shubhanraj2002@gmail.com>
2023-07-09 13:43:19 +0530

    Collections and category model

--- database/migrations/2023_07_09_073944_create_collections_table.php
+++ database/migrations/2023_07_09_073944_create_collections_table.php
@@ -13,7 +13,28 @@ return new class extends Migration
     {
         Schema::create('collections', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->text('short_description');
+            $table->text('long_description');
+            $table->string('url_slug', 100);
+            $table->string('image', 100);
+            $table->string('author_name', 100);
+            $table->integer('author_id');
+            $table->string('tags', 100);
             $table->timestamps();
+        });
+
+        Schema::create('collection_post', function (Blueprint $table) {
+            $table->id();
+            $table->integer('collection_id');
+            $table->integer('post_id');
+            $table->timestamps();
+
+            // Add foreign key constraint for collection_id
+            $table->foreign('collection_id')->references('id')->on('collections');
+
+            // Add foreign key constraint for post_id
+            $table->foreign('post_id')->references('id')->on('posts');
         });
     }
 

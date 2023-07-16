5fb3a2a736f591342c8f3b59710658b2f9ea1162
Subhan Raj <shubhanraj2002@gmail.com>
2023-07-16 11:31:32 +0530

    email template

--- resources/views/emails/otp-email.blade.php
+++ resources/views/emails/otp-email.blade.php
@@ -7,19 +7,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Email Verification</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <style>
+        /* google font */
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
 
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+        p {
+            font-size: 1.1rem;
+            color: #333;
+        }
+    </style>
 </head>
+
 <body>
     <div class="container">
         <div class="row">
             <div class="col-md-6 offset-md-3 mt-5">
                 <div class="card">
                     <div class="card-header">
-                        <h4>Email Verification</h4>
+                        <h2>Email Verification</h2>
                     </div>
                     <div class="card-body">
-                        <p>Hi {{ auth()->user()->name }}</p>
-                        <p>Your email verification otp is <strong>{{ session('otp') }}</strong></p>
+                        <p>Hi {{ auth()->user()->name}},</p>
+                        <p>We are glad to have you on board.<br>In order to complete your registration, please enter the following OTP on the verification page.</p>
+                        <h3 class="text-center" aria-labelledby="Email Verification OTP">{{ session('otp') }}</h3>
+                        <a class="btn btn-primary w-100 py-8 mb-4" href="">Verify My Account</a>
                         <p>Thanks</p>
                     </div>
                 </div>
@@ -27,4 +41,5 @@
         </div>
 </body>@extends('admin.layout')
 @section('content')
+
 </html>

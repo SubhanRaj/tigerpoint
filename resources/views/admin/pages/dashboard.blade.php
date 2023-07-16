9dd79807a9b8a67a4868d4f0164c0a7d9a619f3c
Subhan Raj <shubhanraj2002@gmail.com>
2023-07-16 20:45:53 +0530

    Updated

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -1,41 +1,28 @@
 @extends('admin.layout')
 @section('content')
-@include('admin.includes.sidebar')
-<!--  Main wrapper -->
-<div class="body-wrapper">
-    <!--  Header Start -->
-    @include('admin.includes.header')
-    <!--  Header End -->
-    <!-- Main content -->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="d-flex align-items-center gap-4 mb-4">
-                    <div class="position-relative">
-                        <div class="border border-2 border-primary rounded-circle">
-                            <img src="{{asset('images/admin/profile/user-1.jpg')}}" class="rounded-circle m-1" alt="user1" width="60" />
-                        </div>
-                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary"> 3 <span class="visually-hidden">unread messages</span>
-                        </span>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="d-flex align-items-center gap-4 mb-4">
+                <div class="position-relative">
+                    <div class="border border-2 border-primary rounded-circle">
+                        <img src="{{asset('images/admin/profile/user-1.jpg')}}" class="rounded-circle m-1" alt="user1" width="60" />
                     </div>
-                    <div>
-                        <h3 class="fw-semibold">Hi, <span class="text-dark">
-                           
-                            {{ auth()->user()->name ?? 'Guest' }}                          
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary"> 3 <span class="visually-hidden">unread messages</span>
+                    </span>
+                </div>
+                <div>
+                    <h3 class="fw-semibold">Hi, <span class="text-dark">
+
+                            {{ auth()->user()->name ?? 'Guest' }}
                         </span>
-                        </h3>
-                        <span>Cheers, and happy activities -
-                            {{ date('F d, Y') }}
-                        </span>
-                    </div>
+                    </h3>
+                    <span>Cheers, and happy activities -
+                        {{ date('F d, Y') }}
+                    </span>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Search Bar -->
-    @include('admin.includes.search')
-    <!--  Let user choose dark or light mode -->
-    @include('admin.includes.theme-button')
-
-
-    @endsection
+</div>
+@endsection

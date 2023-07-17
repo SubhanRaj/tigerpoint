3591c3c890e8728db7de8337540fd278de5d7189
Subhan Raj <shubhanraj2002@gmail.com>
2023-07-17 09:19:23 +0530

    form handling

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -17,7 +17,44 @@ class AdminController extends Controller
     }
 
     // Show user profile page
-    public function userprofile(){
+    public function userprofile()
+    {
         return view('admin.pages.user-profile');
+    }
+
+    // Handle user profile update
+    public function updateUserDetails(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'unique:users,email,' . auth()->user()->id],
+            'city' => ['optional', 'string'],
+            'country' => ['optional', 'string'],
+            'mobile' => ['optional', 'string'],
+            'position' => ['optional', 'string'],
+            'address' => ['optional', 'string']
+        ]);
+
+        $user = auth()->user();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        if ($request->has('city')) {
+            $user->city = $request->city;
+        }
+        if ($request->has('country')) {
+            $user->country = $request->country;
+        }
+        if ($request->has('mobile')) {
+            $user->mobile = $request->mobile;
+        }
+        if ($request->has('position')) {
+            $user->position = $request->position;
+        }
+        if ($request->has('address')) {
+            $user->address = $request->address;
+        }
+        $user->save();
+
+        return redirect()->back()->with('success', 'User details updated successfully');
     }
 }
